b43bf3f9c4e7a64f8063bb5243cc69088d71549e
mpreyes <[reyes.madelyn.mr@gmail.com]>
2019-02-22 16:20:36 -0600

    added keras

--- python_scripts copy.py
+++ python_scripts copy.py
@@ -5,11 +5,8 @@ from PIL import Image
 import cv2
 import glob
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from keras.layers import Dense
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-from keras.applications.vgg16 import VGG16
-from keras.preprocessing import image
-from keras.applications.vgg16 import preprocess_input
 
 #create dictionary with filepath, label
 #training data -> labeled
@@ -46,21 +43,21 @@ def getLabels(rootdir,extension): #grabbing labels from labels folder
         for f in files:
             if f.endswith(extension) and not f.startswith("."):
                 f = open(os.path.join(root,f),"r")
-                labels.append(int(float(f.readline().strip("\n"))))
+                labels.append(f.readline())
 
     return labels
 
 
 def resizeImages(img_files,x,y):
         #datagen = ImageDataGenerator(target_size=(64,64))
+
         for i in range(len(img_files)):
             new_im = cv2.imread(img_files[i][1]) #replacing img paths with resized matrix 
-            #k_img = img_to_array(img_files[i][1]) 
-            #print("kmg " + k_img)
-            res = cv2.resize(new_im,dsize=(64,64))
-            #res = res[...,::-1]
+            k_img = img_to_array(img_files[i][1]) 
+            print("kmg " + k_img)
+            res = cv2.resize(new_im,dsize=(64,64)) 
             #k_img = img_to_array(res) 
-            #img_files[i][1] = res
+            img_files[i][1] = res
             x.append(res)
             y.append(img_files[i][0])
         return img_files
@@ -71,53 +68,19 @@ def main():
     labels_dir = "other_images/Emotion/"
     x = [] #inputs (images)
     y = [] #labels 
-    # labels = getLabels(labels_dir,".txt")
-    # img_files = traverseFolders(imgs_dir,".png",labels)
-    # resized_images = resizeImages(img_files,x,y)
-    img_path = 'other_images/cohn-kanade-images/S999/003/S999_003_00000055.png'
-    img = image.load_img(img_path, target_size=(64, 64))
-    img2 = image.img_to_array(img)
-    #x = preprocess_input(x)
-    x.append(np.asarray(img2))
-    y.append(np.asarray(1))
-    #y = to_categorical(y, 7).astype(np.float32)
+    labels = getLabels(labels_dir,".txt")
+    img_files = traverseFolders(imgs_dir,".png",labels)
+    resized_images = resizeImages(img_files,x,y)
     x = np.asarray(x)
-    y = np.asarray(y)
-    print(np.shape(x))
-    print(np.shape(y))
-
+    #y = np.asarray(y)
     model = Sequential()
-    model.add(Dense(units=64, activation='relu',input_shape=(64, 64, 3)))
-    
-
-# #     model = Sequential([
-# #     Flatten(input_shape=(64, 64,3)),
-# #     Dense(128, activation='softmax'),
-# #     #layers.Dense(10, activation=tf.nn.softmax)
-# # ])
-
-#     model.add(Conv2D(32, (3, 3), input_shape=( 64, 64,3)))
-
-    model.add(Dense(units=1, activation='softmax'))
+    model.add(Dense(units=64, activation='relu', input_dim=64))    
+    model.add(Dense(units=10, activation='softmax'))
     model.compile(loss='categorical_crossentropy',
-            optimizer='sgd',
-            metrics=['accuracy'])
-
+              optimizer='sgd',
+              metrics=['accuracy'])
     model.fit(x, y, epochs=5, batch_size=32)
-
-    # model = VGG16(weights='imagenet', include_top=False)
-   
-    
-
-    # #features = model.predict(x)
-    # x = np.expand_dims(x, axis=0)
- 
-
-    # p =  model.fit(x, y, epochs=5, batch_size=32)
-
-    # //print('features: ', features)
-            
-
+        
 
 
 main()
